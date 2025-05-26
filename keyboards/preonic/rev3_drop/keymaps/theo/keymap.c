2f519761625c8c27ab378a24deb71529f05247d2
Theo <theo@iconara.net>
2025-05-26 08:34:26 +0200

    Sync the Preonic layout with the Planck

--- keyboards/preonic/rev3_drop/keymaps/theo/keymap.c
+++ keyboards/preonic/rev3_drop/keymaps/theo/keymap.c
@@ -20,40 +20,65 @@ enum preonic_layers {
 #define QWERTY PDF(_QWERTY)
 #define COLEMAK PDF(_COLEMAK_DH)
 
+#define RAISE_D LT(_RAISE, KC_BSPC)
+#define SFT_GRV MT(MOD_LSFT, KC_GRAVE)
+#define WIN_LFT LCAG(KC_LEFT)
+#define WIN_MAX LCAG(KC_M)
+#define WIN_CEN LCAG(KC_C)
+#define WIN_RGT LCAG(KC_RIGHT)
+
+enum {
+    TD_A,
+    TD_C,
+    TD_F,
+    TD_G,
+    TD_V,
+    TD_Z
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+  [TD_A] = ACTION_TAP_DANCE_DOUBLE(KC_A, LGUI(KC_A)),
+  [TD_C] = ACTION_TAP_DANCE_DOUBLE(KC_C, LGUI(KC_C)),
+  [TD_F] = ACTION_TAP_DANCE_DOUBLE(KC_F, LGUI(KC_F)),
+  [TD_G] = ACTION_TAP_DANCE_DOUBLE(KC_G, LGUI(KC_G)),
+  [TD_V] = ACTION_TAP_DANCE_DOUBLE(KC_V, LGUI(KC_V)),
+  [TD_Z] = ACTION_TAP_DANCE_DOUBLE(KC_Z, LGUI(KC_Z))
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_5x12(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
         NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
     [_COLEMAK_DH] = LAYOUT_ortho_5x12(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
-        KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+        KC_TAB,  KC_Q,    KC_W,    TD(TD_F),KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+        KC_ESC,  TD(TD_A),KC_R,    KC_S,    KC_T,    TD(TD_G),KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        SFT_GRV, TD(TD_Z),KC_X,    TD(TD_C),KC_D,    TD(TD_V),KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE_D, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
     [_LOWER] = LAYOUT_ortho_5x12(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  LSFT(KC_NUHS), LSFT(KC_NUBS), KC_HOME, KC_END, KC_TRNS,
-        _______, _______, _______, _______, _______, _______, _______, ADJUST,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+        KC_DEL,  _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, ADJUST,  WIN_LFT, WIN_MAX, WIN_CEN, WIN_RGT),
     [_RAISE] = LAYOUT_ortho_5x12(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, ADJUST,  _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
     [_ADJUST] = LAYOUT_ortho_5x12(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, QK_BOOT, DB_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-        _______, _______, MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______, _______, _______,
-        _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS),
+        _______, QK_BOOT, DB_TOGG, EE_CLR,  _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, QWERTY,  COLEMAK, _______, _______, _______,
+        _______, MU_ON,   MU_OFF,  MU_NEXT, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
     [_NUMPAD] = LAYOUT_ortho_5x12(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-        _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,  KC_P9,    KC_PMNS, KC_BSPC,
+        _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,  KC_P9,    KC_PMNS, KC_DEL,
         _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,  KC_P6,    KC_PLUS, KC_PSLS,
         _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,  KC_P3,    KC_PAST, KC_PENT,
         _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_P0,  KC_PDOT,  KC_PEQL, _______)
