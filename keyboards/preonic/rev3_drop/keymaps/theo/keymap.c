ef3100677801ecc03c3874fd1c32b50b968237bc
Theo Tolv <tolv@amazon.com>
2025-06-25 13:58:14 +0200

    Add arrow keys for left hand only operation to the Preonic keymap
    This is probably how I want it, no need to have it on the raise layer. Will test and see.

--- keyboards/preonic/rev3_drop/keymaps/theo/keymap.c
+++ keyboards/preonic/rev3_drop/keymaps/theo/keymap.c
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_ortho_5x12(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
         KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        KC_DEL,  _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, ADJUST,  WIN_LFT, WIN_MAX, WIN_CEN, WIN_RGT),
     [_RAISE] = LAYOUT_ortho_5x12(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
